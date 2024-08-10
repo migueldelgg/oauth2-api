1b72a6b6f70b721b78c1754118e81dd671fb4a11
Miguel <delgdevoleper@gmail.com>
2024-08-10 17:18:11 -0300

    Project created

---
+++ src/main/java/github/migueldelgg/springsecurity/entities/Role.java
@@ -0,0 +1,46 @@
+package github.migueldelgg.springsecurity.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private long roleId;
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(long roleId) {
+        this.roleId = roleId;
+    }
+
+    public enum Values{
+        ADMIN(1L),
+        BASIC(2L);
+
+        long roleId;
+
+        Values(long roleId){
+            this.roleId = roleId;
+        }
+
+        public long getRoleId() {
+            return roleId;
+        }
+    }
+}
---
+++ src/main/java/github/migueldelgg/springsecurity/entities/Tweet.java
@@ -0,0 +1,54 @@
+package github.migueldelgg.springsecurity.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_tweets")
+public class Tweet {
+
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "tweet_id")
+    private Long tweetId;
+
+    private User user;
+
+    private String content;
+
+    @CreationTimestamp
+    private Instant creationsTimeStamp;
+
+    public Long getTweetId() {
+        return tweetId;
+    }
+
+    public void setTweetId(Long tweetId) {
+        this.tweetId = tweetId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Instant getCreationsTimeStamp() {
+        return creationsTimeStamp;
+    }
+
+    public void setCreationsTimeStamp(Instant creationsTimeStamp) {
+        this.creationsTimeStamp = creationsTimeStamp;
+    }
+}
